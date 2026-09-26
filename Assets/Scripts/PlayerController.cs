e08a5251acf52b947af630f3fd4077efe29c01d6
Jtad <dat519825@gmail.com>
2026-09-26 18:04:51 +0700

    collision (need fix)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,152 +1,176 @@
 using System.Collections;
+using System.Xml;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour {
-    public float moveSpeed;
-    //private bool isMoving;
-    private Vector2 input;
+	[Header("Movement speed")]
+    public float vx = 10;
+    public float vy = 10;
 
-	
-	public Animator p_ani;
-	
-	public float vx = 10;
-	public float vy = 10;
-	public float transition_time_sec = 1;
+	[Header("Trasition")]
+    public bool activeMovingTransition = false;
+    public float transitionTimeSec = 1;
 
-	public float last_inputx = 0;
-	public float last_inputy = 0;
-	public float last_active_inputx = 0;
-	public float last_active_inputy = 0;	
-    public bool last_moving = false;
-    public bool active_moving_trasition = false;
-	public float input_stop_dt_sec = 0;
+	[Header("Collision")]
+	[SerializeField] private LayerMask solidObjectLayer;
+	[SerializeField] private float collisionCheckRadius;
 
-	private Vector2 get_player_pos(){
-		return transform.position;
+    [Header("Input")]
+    public float inputStopThreshold = 0.1f;
+    private float inputStopTimer = 0;
+	private Vector2 currentInput;
+	private Vector2 lastActiveInput;
+    private bool wasMoving = false;
+    private bool isDecelerating;
+    //private Vector2 input;
+
+    private Animator playerAnimation;
+
+
+    private void Awake(){
+		playerAnimation = GetComponent<Animator>();
 	}
 
-	public float get_dt_frame_sec(){
-		return Time.deltaTime;
-	}
+    private void Update()
+    {
+        float dt = Time.deltaTime;
+        currentInput = ReadBufferedInput(dt);
+        bool isMoving = currentInput != Vector2.zero;
 
-	private void move_player_pos(float dx, float dy){
-		var pos2d = transform.position;
-		pos2d.x += dx;
-		pos2d.y += dy;
-		transform.position = pos2d;
-		return;
-	}
+        if (isMoving)
+            HandleActiveMovement(dt);
+        else
+            HandleStopMovement();
 
-	private void Awake(){
-		this.p_ani = GetComponent<Animator>();
-		return;
-	}
-
-
-
-
-	// rework damn movement input to unified
-
-
-
-    private void Update(){
-        //if (isMoving){ return; }
-		float frame_time = get_dt_frame_sec();
-
-
-		float inputx = Input.GetAxisRaw("Horizontal");
-		float inputy = Input.GetAxisRaw("Vertical");
-
-		if(inputx == 0f && inputy == 0f){
-			if(input_stop_dt_sec < 0.1){
-				input_stop_dt_sec += get_dt_frame_sec();
-			}
-			else{
-				input.x = inputx;
-				input.y = inputy;
-			}
-		}
-		else{
-			this.input_stop_dt_sec = 0;
-			input.x = inputx;
-			input.y = inputy;
-		}
-		//Debug.Log("This is input.x" + input.x);
-		//Debug.Log("This is input.y" + input.y);
-
-		bool check_moving = false;
-		if(input != Vector2.zero){
-			check_moving = true;
-
-			if(!this.last_moving){
-				active_moving_trasition = false;	
-			}
-
-			this.p_ani.SetBool("is_moving", true);
-			this.p_ani.SetFloat("mx", input.x);
-			this.p_ani.SetFloat("my", input.y);
-
-			//move_player_pos(input.x, input.y);
-			//StartCoroutine(Move(targetPos)); // this line to make player move
-			//StartCoroutine(handle_player_movement(input.x * 2, input.y * 2, 1)); // this line to make player move
-
-			move_player_pos(input.x * vx * frame_time, input.y * vy * frame_time);
-			this.last_active_inputx = input.x;
-			this.last_active_inputy = input.y;
-		}
-		else{
-			// will continue normal movement on not enough stop time, else true stop
-			if(!active_moving_trasition){
-				active_moving_trasition = true;
-				StartCoroutine(handle_player_movement_transition(last_active_inputx * vx, last_active_inputy * vy, transition_time_sec));
-				Debug.Log("is stopping");		
-			}
-		}
-		
-		this.last_inputx = input.x;
-		this.last_inputy = input.y;
-		this.last_moving = check_moving;
-		return;
+        wasMoving = isMoving;
     }
 
 
+    //Input
+    private Vector2 ReadBufferedInput(float dt)
+    {
+        float rawX = Input.GetAxis("Horizontal");
+        float rawY = Input.GetAxis("Vertical");
+        bool hasRawInput = rawX != 0f || rawY != 0f;
 
-	
-	// buggy and outdated, still need to research sth here
-	/*
-    private IEnumerator Move(Vector3 targetPos){
-        isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon){
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        //has input
+        if (hasRawInput)
+        {
+            inputStopTimer = 0f;
+            return new Vector2(rawX, rawY);
+        }
+        //transition before stop
+        if (inputStopTimer < inputStopThreshold)
+        {
+            inputStopTimer += dt;
+            return currentInput;
+        }
+        return Vector2.zero;
+    }
+
+    //Handler
+    private void HandleActiveMovement(float dt)
+    {
+        if (!wasMoving)
+            isDecelerating = false;
+
+        lastActiveInput = currentInput;
+        UpdateAnimation(currentInput, true);
+        TryMove(currentInput.x * vx * dt, currentInput.y * vy * dt);
+    }
+
+    private void HandleStopMovement()
+    {
+        if (isDecelerating)
+            return;
+
+        isDecelerating = true;
+        float startVx = lastActiveInput.x * vx;
+        float startVy = lastActiveInput.y * vy;
+        StartCoroutine(DecelerateCoroutine(startVx, startVy, transitionTimeSec));
+    }
+
+
+    //Slow down coroutine
+    private IEnumerator DecelerateCoroutine(float startVx, float startVy, float duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            if (!isDecelerating)
+                yield break;
+            float dt = Time.deltaTime;
+            elapsed += dt;
+
+            float t = 1f - (elapsed / duration);
+            t += t;
+
+            bool moved = TryMove(startVx * t * dt, startVy * t * dt);
+
+            if (t <= 0.1f || !moved)
+            {
+                UpdateAnimation(Vector2.zero, false);
+                if (!moved)
+                    yield break;
+            }
+               
             yield return null;
         }
-        transform.position = targetPos;
-        isMoving = false;
-		// end func
-    }
-	*/
-	
-	IEnumerator handle_player_movement_transition(float vx, float vy, float dt_sec){
-		Debug.Log("is stopping start");
-		//this.isMoving = true;
-		
-		float orig_vx = vx;
-		float orig_vy = vy;
-		float dt_current = 0;
-		for(;dt_current < dt_sec;){
-			if(!active_moving_trasition){ yield break; }
-			float frame_time = get_dt_frame_sec();
-			dt_current += frame_time;
-			float t = 1f - (dt_current / dt_sec); // linear ease-out
-			t = t * t;
-			move_player_pos(orig_vx * t * frame_time, orig_vy * t * frame_time);
-			Debug.Log("is stopping vx " + orig_vx * t + " vy " + orig_vy * t);
-			if(t <= 0.1){ this.p_ani.SetBool("is_moving", false); }
-			yield return null;
-		}
-        //isMoving = false;
 	}
+
+    //Move and Collision
+
+    //check each exis
+    private bool TryMove(float dx, float dy)
+    {
+        Vector3 targetPos = transform.position;
+        bool moved = false;
+        if (dx != 0f)
+        {
+            Vector3 targetX = targetPos;
+            targetX.x += dx;
+            if (IsWalkable(targetX))
+            {
+                targetPos.x = targetX.x;
+                moved = true;
+            }
+        }
+        if (dy != 0f)
+        {
+            Vector3 targetY = targetPos;
+            targetY.y += dy;
+            if (IsWalkable(targetY))
+            {
+                targetPos.y = targetY.y;
+                moved = true;
+            }
+        }
+        
+        transform.position = targetPos;
+        return moved;
+    }
+
+
+	private bool IsWalkable(Vector3 targetPos)
+	{
+		if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectLayer))
+			return false;
+		return true;
+	}
+    
+    // Play animation
+    private void UpdateAnimation(Vector2 direction, bool moving)
+    {
+        playerAnimation.SetBool("is_moving", moving);
+
+        if (moving)
+        {
+            playerAnimation.SetFloat("mx", direction.x);
+            playerAnimation.SetFloat("my", direction.y);
+        }
+    }
 
 
 }
